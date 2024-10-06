1da63515816839b7c71fd3642b9ce261acc1984e
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2024-10-06 12:02:57 +0200

    Polished the header

--- src/images.js
+++ src/images.js
@@ -8,9 +8,22 @@ function createImage(source, ...classes) {
     return img;
 }
 
+function create(type, ...classes) {
+    const element = document.createElement(type);
+    element.classList.add(...classes);
+    return element;
+}
+
 const logo = createImage(odinLogo, 'logo');
+const titleBox = create('div', 'titleBox');
+const titleMain = create('div', 'titleMain');
+titleMain.textContent = "Pizzeria da Odino";
+const subtitle = create('div', 'subtitle');
+subtitle.textContent = 'The best pizza in all the nine realms!';
 
 export default (() => {
     sel.header.insertBefore(logo, sel.nav);
+    sel.header.appendChild(titleBox);
+    titleBox.append(titleMain, subtitle, sel.nav);
 })();
 
--- src/selectors.js
+++ src/selectors.js
@@ -1,5 +1,6 @@
 const header = document.querySelector('header');
 const nav = document.querySelector('nav');
 const content = document.getElementById('content');
+const doc = document;
 
-export {header, nav, content};
+export {header, nav, content, doc};
